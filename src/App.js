31e1a7e701c90529453dd3b625e6f4544de39218
Francis Maina <sebastianfrancix@gmail.com>
2023-02-17 12:11:52 +0300

    added routes for our components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Box } from "@mui/material"; //Box is a Simple div element from MUI
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+
+        {/* Routes for our components */}
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
